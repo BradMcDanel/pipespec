bb253478d7c6bb2cdf2307a90073beab2f23ecb3
bmcdanel@fandm.edu <mcdabw6@gmail.com>
2024-10-13 18:00:04 -0400

    .

--- test_amusd.py
+++ test_amusd.py
@@ -7,21 +7,19 @@ from datasets import load_from_disk
 
 if __name__ == "__main__":
     mp.set_start_method('spawn')
-
     
-    mp2 = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:0", torch.float32)
-    decoder = amusd.GreedyDecoder(mp2)
+    # verify = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:0", torch.float32)
+    # decoder = amusd.GreedyDecoder(verify)
 
-    # mp1 = amusd.ModelConfig("meta-llama/Llama-3.2-1B-Instruct", "cuda:0", torch.float32)
-    # mp2 = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:0", torch.float32)
-    # decoder = amusd.SyncSpeculativeDecoder(mp1, mp2)
+    # draft = amusd.ModelConfig("meta-llama/Llama-3.2-1B-Instruct", "cuda:0", torch.float32)
+    # verify = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:0", torch.float32)
+    # decoder = amusd.SyncSpeculativeDecoder(draft, verify)
 
-    # mp1 = amusd.ModelConfig("meta-llama/Llama-3.2-1B-Instruct", "cuda:0", torch.float32)
-    # mp2 = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:1", torch.float32)
-    # decoder = amusd.AsyncMultiGPUSpeculativeDecoder(mp1, mp2)
-
+    draft = amusd.ModelConfig("meta-llama/Llama-3.2-1B-Instruct", "cuda:1", torch.float32)
+    verify = amusd.ModelConfig("meta-llama/Llama-3.1-8B-Instruct", "cuda:0", torch.float32)
+    decoder = amusd.AsyncMultiGPUSpeculativeDecoder(draft, verify)
     
-    tok = AutoTokenizer.from_pretrained(mp2.model_path)
+    tok = AutoTokenizer.from_pretrained(verify.model_path)
     
     conversation = [{"role": "user", "content": "Who wrote the Illiad and where?"}]
     input_ids = tok.apply_chat_template(conversation, return_tensors="pt")
@@ -31,5 +29,5 @@ if __name__ == "__main__":
     print(tok.decode(output_ids, skip_special_tokens=False))
     print(metrics)
 
-    # decoder.close()
+    decoder.close()
 
